c37202ca5507be04f8e81ebefacd254ae42277b7
Boris <marco.arka@gmail.com>
2020-01-29 00:18:53 -0500

    WIP navigation: create signin, signup and nav_graph

---
+++ app/src/main/java/com/mmaquera/netgame/view/login/signin/SignInFragment.kt
@@ -0,0 +1,26 @@
+package com.mmaquera.netgame.view.login.signin
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.mmaquera.netgame.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SignInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    }
+
+
+}
