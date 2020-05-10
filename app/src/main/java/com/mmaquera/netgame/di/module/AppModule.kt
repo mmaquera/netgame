88f3e0b51fc5377209e7040039c6fea9ef3f7d9f
Boris <marco.arka@gmail.com>
2020-05-10 11:27:34 -0500

    WIP setup config

--- app/src/main/java/com/mmaquera/netgame/di/module/AppModule.kt
+++
@@ -1,6 +0,0 @@
-package com.mmaquera.netgame.di.module
-
-import dagger.Module
-
-@Module
-class AppModule
---
+++ app/src/main/java/com/mmaquera/netgame/view/login/signin/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package com.mmaquera.netgame.view.login.signin
+
+import androidx.lifecycle.ViewModel
+
+class SignInViewModel : ViewModel() {
+
+}
